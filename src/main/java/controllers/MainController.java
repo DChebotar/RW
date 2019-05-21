8e004fd69b23c4ffe3bc9efb10e7656f0cc80583
Chebotar Dmitrij Olegovich <Chebotar_do@sbyt.minsk.energo.net>
2019-05-21 16:53:45 +0300

    Initial commit

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Chebotar_do on 21.05.2019.
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String start(){
+        return "index";
+    }
+}
